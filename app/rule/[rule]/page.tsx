62cb965e4990a17486d25750be414fddb2bb389e
Brook Jeynes <jeynesbrook@gmail.com>
2024-02-21 09:22:07 +1000

    updated renderer

--- app/rule/[rule]/page.tsx
+++ app/rule/[rule]/page.tsx
@@ -2,12 +2,27 @@ import { TinaMarkdown } from "tinacms/dist/rich-text";
 import client from "@/tina/__generated__/client";
 
 async function Page({ params }: { params: { rule: any } }) {
-    const ruleResponse = await client.queries.rule({ relativePath: `${params.rule}.mdx` });
-    const rule = ruleResponse.data.rule;
+    let rule;
+    let ruleMd;
+
+    try {
+        const ruleResponse = await client.queries.rule({ relativePath: `${params.rule}.mdx` });
+        rule = ruleResponse.data.rule;
+    } catch (e) {
+        console.error(e);
+    }
+
+    try {
+        const ruleResponseMd = await client.queries.rule({ relativePath: `${params.rule}.md` });
+        ruleMd = ruleResponseMd.data.rule;
+    } catch (e) {
+        console.error(e);
+    }
 
     return (
         <div>
-            <TinaMarkdown content={rule.body} />
+            {rule && <TinaMarkdown content={rule.body} />}
+            {ruleMd && <TinaMarkdown content={ruleMd.body} />}
         </div>
     );
 }
