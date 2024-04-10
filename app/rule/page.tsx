777c7fc14669f8ce305d5a8d7a1bbff4e4a5e9fb
Brook J <jeynesbrook@gmail.com>
2024-04-10 15:02:36 +1000

    pagination

--- app/rule/page.tsx
+++ app/rule/page.tsx
@@ -2,14 +2,25 @@ import { TinaMarkdown } from "tinacms/dist/rich-text";
 import client from "@/tina/__generated__/client";
 
 async function Rules() {
-    const rulesResponse = await client.queries.ruleConnection({ first: 100 });
+    let cursor = null;
+    let hasNextPage = true;
+
+    let pages = [];
+
+    while (hasNextPage) {
+        const rulesResponse = await client.queries.ruleConnection({ first: 50, after: cursor });
+        cursor = rulesResponse.data.ruleConnection.pageInfo.endCursor;
+        hasNextPage = rulesResponse.data.ruleConnection.pageInfo.hasNextPage;
+
+        pages.push(rulesResponse.data.ruleConnection.edges);
+    }
 
     return (
         <div>
             <a href="http://localhost:3000/admin/index.html#/collections/edit/rule/Test-rule-1">test</a>
             <ul>
                 {
-                    rulesResponse.data.ruleConnection.edges?.map(rule => <li key={rule?.node?._sys.filename}>
+                    pages.flat().map(rule => <li key={rule?.node?._sys.filename}>
                         <p key={rule?.node?.id}>{rule?.node?.title}</p>
                     </li>)
                 }
